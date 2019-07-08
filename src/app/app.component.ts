1df65e01789593c4620cadbbbb6f7588f11d5971
TropintheBard <23250543+TropintheBard@users.noreply.github.com>
2019-07-08 16:03:24 -0700

    "Wrote a basic Form"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reactive-forms-project';
+  accountType =['Personal', 'Buisiness'];
+  signupForm: FormGroup;
+
+  ngOnInit(){
+    this.signupForm = new FormGroup({
+      'accountType': new FormControl('Personal'),
+      'name': new FormControl(),
+      'email': new FormControl(),
+      'streetAddress': new FormControl(),
+      'city': new FormControl(),
+      'country': new FormControl(),
+      'zipCode': new FormControl(),
+      'favoriteTextile': new FormControl(),
+      'favoriteSauce': new FormControl(),
+    })
+  }
+
+  onSubmit(){
+    console.log(this.signupForm);
+    
+  }
 }
