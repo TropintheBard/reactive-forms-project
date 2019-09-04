da347fc491f755c5569fd202f08f48a413114fae
TropintheBard <23250543+TropintheBard@users.noreply.github.com>
2019-09-04 14:12:14 -0700

    save updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -14,19 +15,21 @@ export class AppComponent implements OnInit {
   ngOnInit(){
     this.signupForm = new FormGroup({
       'accountType': new FormControl('Personal'),
-      'name': new FormControl(),
-      'email': new FormControl(),
-      'streetAddress': new FormControl(),
-      'city': new FormControl(),
-      'country': new FormControl(),
-      'zipCode': new FormControl(),
-      'favoriteTextile': new FormControl(),
-      'favoriteSauce': new FormControl(),
+      'name': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'streetAddress': new FormControl(null),
+      'city': new FormControl(null),
+      'country': new FormControl(null, Validators.required),
+      'zipCode': new FormControl(null, [Validators.pattern("[0-9]*"),Validators.maxLength(6)]),
+      'favoriteTextile': new FormControl(null, Validators.required),
+      'favoriteSauce': new FormControl(null, Validators.required),
+      'favoriteWayToDie': new FormControl(null)
     })
   }
 
   onSubmit(){
     console.log(this.signupForm);
-    
+    console.log(this.signupForm.value);
+
   }
 }
